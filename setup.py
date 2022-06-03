a9879a671031092ec937ed64c97896435b60173c
sergiomarco25 <55990938+sergiomarco25@users.noreply.github.com>
2022-06-03 10:16:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,2 +1,37 @@
-from setuptools import setup
-setup(name='plp_design',version='0.1.0', description='Package to design Padlock probes based on a reference transcriptome', url='https://github.com/Moldia/PLP_directRNA_design',author_email='sergiomarco.salas@scilifelab.se',license='BSD 2-clause',install_requires=['pandas>=1.1.5','Bio>=1.0','numpy>=1.19.0' ])#install_requires=['pandas==1.1.5','Bio==1.79','numpy==1.19.4' ])
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.1.1'
+DESCRIPTION = 'Package to design Padlock probes based on a reference transcriptome'
+LONG_DESCRIPTION = 'Package to design Padlock probes based on a reference transcriptome'
+
+# Setting up
+setup(
+    name="PLP_directRNA_desing",
+    version=VERSION,
+    author="Sergio Marco Salas",
+    author_email="<sergiomarco.salas@scilifelab.se>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=['pandas>=1.1.5','Bio>=1.0','numpy>=1.19.0'],
+    keywords=['python', 'spatial transcriptomics', 
+            'spatial resolved transcriptomics', 
+            'in situ sequencing', 
+            'ISS','padlock probe design'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Researchers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
